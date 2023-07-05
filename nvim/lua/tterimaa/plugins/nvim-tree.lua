12f1e99176f10a6845559a99d7900d1807ff0998
Tarmo Terimaa <tarmo.terimaa@efecte.com>
2023-07-05 10:18:25 +0300

    Nvim config files

---
+++ nvim/lua/tterimaa/plugins/nvim-tree.lua
@@ -0,0 +1,11 @@
+-- import nvim-tree plugin safely
+local setup, nvimtree = pcall(require, "nvim-tree")
+if not setup then
+  return
+end
+
+-- recommended settings from nvim-tree documentation
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+nvimtree.setup()
